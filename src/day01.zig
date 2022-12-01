c10207097076d133c1ebde9318f73c1352fa62be
David Virgilio <5187363+drvirgilio@users.noreply.github.com>
2022-12-01 15:50:41 -0600

    day 1

--- src/day01.zig
+++ src/day01.zig
@@ -9,9 +9,87 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day01.txt");
+const example =
+\\1000
+\\2000
+\\3000
+\\
+\\4000
+\\
+\\5000
+\\6000
+\\
+\\7000
+\\8000
+\\9000
+\\
+\\10000
+;
 
 pub fn main() !void {
+    const Item = struct{index: u32, value: u32};
+    const input: []Item = blk: {
+        var iter = split(u8, data, "\n");
+        var list = List(Item).init(gpa);
+        var index: u32 = 0;
+        while (iter.next()) |s| {
+            const st = trim(u8, s, "\r");
+            if (st.len == 0) {
+                // This is the end of the list for this particular elf
+                // Increment index and go to next line
+                index += 1;
+            } else {
+                const n = try parseInt(u32, st, 10);
+                try list.append(.{.index = index, .value = n });
+            }
+        }
+        break :blk list.toOwnedSlice();
+    };
     
+    // part 1
+    {
+        // sum the items grouped by index
+        var sums = List(u64).init(gpa);
+        for (input) |item| {
+            if (item.index >= sums.items.len) {
+                try sums.append(item.value);
+            } else {
+                sums.items[sums.items.len-1] += item.value;
+            }
+        }
+
+        // find the maximum value
+        var answer: u64 = 0;
+        for (sums.items) |value| {
+            answer = max(answer, value);
+        }
+
+        print("{}\n", .{answer});
+    }
+
+    // part 2
+    {
+        // sum the items grouped by index
+        var sums = List(u64).init(gpa);
+        for (input) |item| {
+            if (item.index >= sums.items.len) {
+                try sums.append(item.value);
+            } else {
+                sums.items[sums.items.len-1] += item.value;
+            }
+        }
+
+        // sort the sums
+        sort(u64, sums.items, {}, comptime desc(u64));
+
+        // sum the maximum 3 values
+        var answer: u64 = 0;
+        for (sums.items[0..3]) |value| {
+            answer += value;
+        }
+
+        print("{}\n", .{answer});
+    }
 }
 
 // Useful stdlib functions
