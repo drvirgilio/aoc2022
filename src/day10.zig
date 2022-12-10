f78f6857ff61cb58f736bed51dea7a68d9f3728e
David Virgilio <5187363+drvirgilio@users.noreply.github.com>
2022-12-10 12:07:57 -0600

    day 10 ocr

--- src/day10.zig
+++ src/day10.zig
@@ -29,26 +29,25 @@ pub fn main() !void {
         break :blk list.toOwnedSlice();
     };
 
-    print("part 2:\n", .{});
-
     var answer_1: isize = 0;
     var clk: isize = 0; // using 0-indexing instead of 1-indexing
     var x: isize = 1;
+    var buffer = List(u8).init(gpa);
     for (input) |inst| {
         switch (inst) {
             .noop => {
-                display(clk, x);
+                try display(clk, x, &buffer);
                 answer_1 += signal_strenth(clk, x);
                 clk += 1;
 
                 continue;
             },
             .addx => |amount| {
-                display(clk, x);
+                try display(clk, x, &buffer);
                 answer_1 += signal_strenth(clk, x);
                 clk += 1;
 
-                display(clk, x);
+                try display(clk, x, &buffer);
                 answer_1 += signal_strenth(clk, x);
                 clk += 1;
 
@@ -58,16 +57,24 @@ pub fn main() !void {
         }
     }
 
-    print("part 1: {}\n", .{answer_1});
+    const answer_2: []u8 = try detect_letters(gpa, buffer.items);
+
+    print("{}\n", .{answer_1});
+    //print("{s}\n", .{buffer.items});
+    print("{s}\n", .{answer_2});
+
+    assert(answer_1 == 12560);
+    assert(std.mem.eql(u8, answer_2, "PLPAFBCL"));
 }
 
-fn display(clk: isize, x: isize) void {
+fn display(clk: isize, x: isize, buffer: *List(u8)) !void {
     const pixel = @mod(clk, 40);
-    switch (x - pixel) {
-        -1, 0, 1 => print("#", .{}),
-        else => print(" ", .{}),
-    }
-    if (pixel == 39) print("\n", .{});
+    const char: u8 = switch (x - pixel) {
+        -1, 0, 1 => '#',
+        else => ' ',
+    };
+    try buffer.append(char);
+    if (pixel == 39) try buffer.append('\n');
 }
 
 fn signal_strenth(clk: isize, x: isize) isize {
@@ -75,6 +82,69 @@ fn signal_strenth(clk: isize, x: isize) isize {
         (clk + 1) * x
     else
         0;
+}
+
+const alphabet = "ABCEFGHIJKLOPRSUYZ";
+
+const alphabet_font = [_][]const u8{
+    " ## \n#  #\n#  #\n####\n#  #\n#  #",
+    "### \n#  #\n### \n#  #\n#  #\n### ",
+    " ## \n#  #\n#   \n#   \n#  #\n ## ",
+    "####\n#   \n### \n#   \n#   \n####",
+    "####\n#   \n### \n#   \n#   \n#   ",
+    " ## \n#  #\n#   \n# ##\n#  #\n ###",
+    "#  #\n#  #\n####\n#  #\n#  #\n#  #",
+    " ###\n  # \n  # \n  # \n  # \n ###",
+    "  ##\n   #\n   #\n   #\n#  #\n ## ",
+    "#  #\n# # \n##  \n# # \n# # \n#  #",
+    "#   \n#   \n#   \n#   \n#   \n####",
+    " ## \n#  #\n#  #\n#  #\n#  #\n ## ",
+    "### \n#  #\n#  #\n### \n#   \n#   ",
+    "### \n#  #\n#  #\n### \n# # \n#  #",
+    " ###\n#   \n#   \n ## \n   #\n### ",
+    "#  #\n#  #\n#  #\n#  #\n#  #\n ## ",
+    "#   \n#   \n # #\n  # \n  # \n  # ",
+    "####\n   #\n  # \n #  \n#   \n####",
+};
+
+fn detect_letters(allocator: std.mem.Allocator, buffer: []u8) ![]u8 {
+    var output = List(u8).init(allocator);
+    const display_width = std.mem.indexOf(u8, buffer, "\n").?;
+    const letter_width = 5;
+    const letter_height = 6;
+    const num_letters = display_width / letter_width;
+
+    var i: usize = 0;
+    while (i < num_letters) : (i += 1) {
+        const x: usize = i * letter_width;
+        var y: usize = 0;
+        var letter: [letter_height][]const u8 = undefined;
+        var lines = tokenize(u8, buffer, "\n");
+        while (lines.next()) |line| : (y += 1) {
+            letter[y] = line[x .. x + letter_width - 1]; // don't include last column of letter
+        }
+
+        // print single letter to buffer
+        var letter_print: [letter_height * letter_width]u8 = undefined;
+        for (letter) |row_data, row| {
+            for (row_data) |char, col| {
+                const index = row * letter_width + col;
+                letter_print[index] = char;
+            }
+            letter_print[(row + 1) * letter_width - 1] = '\n';
+        }
+
+        // compare printed letter to alphabet_font
+        const character = for (alphabet_font) |letter_font, alphabet_index| {
+            if (std.mem.eql(u8, letter_font, letter_print[0 .. letter_print.len - 1])) {
+                break alphabet[alphabet_index];
+            }
+        } else '?';
+
+        try output.append(character);
+    }
+
+    return output.toOwnedSlice();
 }
 
 // Useful stdlib functions
