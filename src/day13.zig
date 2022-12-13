16953353bdede8c24f66ed883efd8105c8150b73
David Virgilio <5187363+drvirgilio@users.noreply.github.com>
2022-12-13 15:25:05 -0600

    day 13

--- src/day13.zig
+++ src/day13.zig
@@ -45,7 +45,6 @@ const Token = union(TokenType) {
 
     pub fn tokenize(allocator: std.mem.Allocator, input: []const u8) ![]Self {
         var list = List(Token).init(allocator);
-        //var depth: usize = 0;
         var i: usize = 0;
         while (i < input.len) : (i += 1) {
             const this: u8 = input[i];
@@ -89,12 +88,11 @@ const NodeType = enum { list, integer };
 const Node = union(NodeType) {
     const Self = @This();
 
-    list: []*Node,
+    list: []*const Node,
     integer: usize,
 
     // pub fn print(self: Self, allocator: std.mem.Allocator) !void {
     pub fn print(self: *const Self) void {
-        // var stack = List(Self).init(allocator);
         switch (self.*) {
             .integer => |n| std.debug.print(" {d} ", .{n}),
             .list => |nodes| {
@@ -110,7 +108,7 @@ const Node = union(NodeType) {
     /// last node in the output slice is the root node
     fn parseTokens(allocator: std.mem.Allocator, tokens: []Token) ![]const Self {
         var nodes = try List(Self).initCapacity(allocator, tokens.len);
-        //defer nodes.deinit();
+        defer nodes.deinit();
         var stack = List(List(*Self)).init(allocator);
         defer stack.deinit();
         var i: usize = 0;
@@ -124,8 +122,6 @@ const Node = union(NodeType) {
                 .right_bracket => {
                     // std.debug.print("]", .{});
                     // pop list off stack, create list node, commit it to nodes list, and add pointer to list on top of stack (parent)
-                    // var list: List(*Self) = try stack.pop().clone();
-                    // const node = Node{ .list = list.toOwnedSlice() };
                     const node = Node{ .list = (try stack.pop().clone()).toOwnedSlice() };
                     try nodes.append(node);
                     if (stack.items.len > 0) {
@@ -145,27 +141,128 @@ const Node = union(NodeType) {
         // std.debug.print("\n", .{});
         return nodes.toOwnedSlice();
     }
+
+    pub fn asc(context: void, left: *const Self, right: *const Self) bool {
+        _ = context;
+        return switch (left.order(right)) {
+            .lt => true,
+            .eq => false,
+            .gt => false,
+        };
+    }
+
+    pub fn order(left: *const Self, right: *const Self) std.math.Order {
+        return switch (left.*) {
+            .integer => |l| switch (right.*) {
+                .integer => |r| std.math.order(l, r),
+                .list => blk: {
+                    var array: [1]*const Self = [1](*const Self){left};
+                    const new_left = Self{ .list = &array };
+                    break :blk new_left.order(right);
+                },
+            },
+            .list => |l| switch (right.*) {
+                .integer => blk: {
+                    var array = [1]*const Self{right};
+                    const new_right = Self{ .list = &array };
+                    break :blk left.order(&new_right);
+                },
+                .list => |r| blk: {
+                    const length = std.math.min(l.len, r.len);
+                    var i: usize = 0;
+                    while (i < length) : (i += 1) {
+                        const compare = l[i].order(r[i]);
+                        switch (compare) {
+                            .eq => continue,
+                            else => break :blk compare,
+                        }
+                    } else {
+                        break :blk std.math.order(l.len, r.len);
+                    }
+                },
+            },
+        };
+    }
 };
 
 pub fn main() !void {
-    var lines = tokenize(u8, example, "\r\n");
-    // var lines = tokenize(u8, data, "\r\n");
+    // part 1
+    {
+        // var lines = tokenize(u8, example, "\r\n");
+        var lines = tokenize(u8, data, "\r\n");
 
-    while (lines.next()) |fst_line| {
-        const snd_line = lines.next().?;
-        const fst_tokens = try Token.tokenize(gpa, fst_line);
-        const snd_tokens = try Token.tokenize(gpa, snd_line);
+        var index: usize = 1;
+        var answer_1: usize = 0;
+        while (lines.next()) |fst_line| : (index += 1) {
+            const snd_line = lines.next().?;
+            const fst_tokens = try Token.tokenize(gpa, fst_line);
+            const snd_tokens = try Token.tokenize(gpa, snd_line);
 
-        const fst_nodes = try Node.parseTokens(gpa, fst_tokens);
-        const snd_nodes = try Node.parseTokens(gpa, snd_tokens);
+            defer gpa.free(fst_tokens);
+            defer gpa.free(snd_tokens);
 
-        const fst = fst_nodes[fst_nodes.len - 1];
-        const snd = snd_nodes[snd_nodes.len - 1];
+            const fst_nodes = try Node.parseTokens(gpa, fst_tokens);
+            const snd_nodes = try Node.parseTokens(gpa, snd_tokens);
 
-        fst.print();
-        print("\n", .{});
-        snd.print();
-        print("\n\n", .{});
+            defer gpa.free(fst_nodes);
+            defer gpa.free(snd_nodes);
+
+            const fst = fst_nodes[fst_nodes.len - 1];
+            const snd = snd_nodes[snd_nodes.len - 1];
+
+            answer_1 += switch (fst.order(&snd)) {
+                .lt => blk: {
+                    break :blk index;
+                },
+                .gt => 0,
+                .eq => unreachable,
+            };
+        }
+
+        print("{d}\n", .{answer_1});
+    }
+
+    // part 2
+    {
+        // var lines = tokenize(u8, example, "\r\n");
+        var lines = tokenize(u8, data, "\r\n");
+
+        var roots = List(*const Node).init(gpa);
+        defer roots.deinit();
+        while (lines.next()) |line| {
+            const tokens = try Token.tokenize(gpa, line);
+            const nodes = try Node.parseTokens(gpa, tokens);
+            const root = &nodes[nodes.len - 1];
+
+            defer gpa.free(tokens);
+
+            // this causes use-after-free bug because we give the memory to roots
+            // instead this is a memory leak
+            // TODO: find a way to free this memory outside the while loop
+            // defer gpa.free(nodes);
+
+            try roots.append(root);
+        }
+
+        // append divider packets
+        // TODO: free this memory
+        const p1_tokens = try Token.tokenize(gpa, "[[2]]");
+        const p2_tokens = try Token.tokenize(gpa, "[[6]]");
+        const p1_nodes = try Node.parseTokens(gpa, p1_tokens);
+        const p2_nodes = try Node.parseTokens(gpa, p2_tokens);
+        const p1 = &p1_nodes[p1_nodes.len - 1];
+        const p2 = &p2_nodes[p2_nodes.len - 1];
+        try roots.append(p1);
+        try roots.append(p2);
+
+        std.sort.sort(*const Node, roots.items, {}, comptime Node.asc);
+        var answer_2: usize = 1;
+        for (roots.items) |root, index| {
+            if (root == p1) answer_2 *= index + 1;
+            if (root == p2) answer_2 *= index + 1;
+        }
+
+        print("{d}\n", .{answer_2});
     }
 }
 
